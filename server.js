e15c6244a606dbad0effb9e4ac53e07de79691e5
Cabral Williams <cabral.williams@gmail.com>
2021-10-25 11:03:15 -0500

    first query added

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
+require('dotenv').config();
 
 const express = require('express');
 const PORT = process.env.PORT || 3005;
 const app = express();
+const mysql = require('mysql2');
+const myPassword = process.env.DB_PASSWORD;
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: myPassword,
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World'
     });
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 // Default response for any other request (Not Found)
